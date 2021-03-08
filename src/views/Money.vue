0a4213f7ae13bb86a4ae9818ce3deee317cd9326
nicole <1251501326@qq.com>
2021-03-08 17:46:24 +0800

    Money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Money.vue
+    </div>
     <Nav/>
   </div>
 </template>
@@ -13,5 +16,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+   .nav-wrapper{
+     display: flex;
+     flex-direction: column;
+     height: 100vh;
+   }
+   .content{
+     flex-grow: 1;
+     overflow: hidden;
+   }
 </style>
