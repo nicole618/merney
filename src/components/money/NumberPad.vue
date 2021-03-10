b7bae39fd0ea2d3b180f446fb694fe606172345f
nicole <1251501326@qq.com>
2021-03-10 13:34:40 +0800

    numberPad模块基本完成

--- src/components/money/NumberPad.vue
+++ src/components/money/NumberPad.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="numberPad">
-    <div class="output"><span>100</span></div>
-    <div class="buttons clearfix">
+    <div class="output"><span>{{ output }}</span></div>
+    <div class="buttons clearfix" @click="inputContent">
       <button>1</button>
       <button>2</button>
       <button>3</button>
-      <button>删除</button>
+      <button @click="remove">
+        <Icon name="delete"/>
+      </button>
       <button>4</button>
       <button>5</button>
       <button>6</button>
@@ -13,7 +15,7 @@
       <button>7</button>
       <button>8</button>
       <button>9</button>
-      <button class="ok">OK</button>
+      <button class="ok" @click="ok">OK</button>
       <button class="zero">0</button>
       <button class="dot">.</button>
     </div>
@@ -21,56 +23,109 @@
 </template>
 
 <script lang="ts">
-export default {
-name: "NumberPad"
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class NumberPad extends Vue {
+  output = '0';
+
+  inputContent(event: MouseEvent) {
+    const button = event.target as HTMLButtonElement;
+    const input = button.textContent as string;
+    switch (input) {
+      case '0':
+        if (this.output === '0') return;
+        this.output += input;
+        break;
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        this.output === '0'?this.output = input:this.output+=input;
+        break
+      case '.':
+        if(this.output.indexOf('.')>=0)return;
+        this.output += input;
+        break;
+      case '清空':
+        this.output = '0';
+        break;
+
+    }
+    }
+  remove(){
+    if (this.output.length >1){
+      this.output = this.output.slice(0,-1);
+    }else{
+      this.output = '0';
+    }
+  }
+  ok(){
+    return;
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
-.numberPad{
-  >.output{
-    padding: 0 16px;
-    padding: 10px 0;
+
+.numberPad {
+  > .output {
+    padding: 10px 16px;
     display: flex;
     justify-content: flex-end;
     font-size: 26px;
   }
-  >.buttons{
-    button{
+
+  > .buttons {
+    button {
       width: 25%;
       font-size: 16px;
       border: none;
       float: left;
       background: none;
       height: 35px;
-      background:$color-Light;
+      background: $color-Light;
       display: flex;
       justify-content: center;
       align-items: center;
-      &:nth-child(1){
+
+      &:nth-child(1) {
         background: #f3eeeb;
       }
-      &:nth-child(2),&:nth-child(5){
+
+      &:nth-child(2), &:nth-child(5) {
         background: #f3e9e3;
       }
-      &:nth-child(3),&:nth-child(6),&:nth-child(9){
+
+      &:nth-child(3), &:nth-child(6), &:nth-child(9) {
         background: #f1e2d9;
       }
-      &:nth-child(4),&:nth-child(7),&:nth-child(10),&:nth-child(13){
+
+      &:nth-child(4), &:nth-child(7), &:nth-child(10), &:nth-child(13) {
         background: #e8d9d0;
       }
-      &:nth-child(8),&:nth-child(11){
+
+      &:nth-child(8), &:nth-child(11) {
         background: #e4d1c6;
       }
-      &:nth-child(12),&:nth-child(14){
+
+      &:nth-child(12), &:nth-child(14) {
         background: #e4ccbe;
       }
-      &.ok{
+
+      &.ok {
         height: 70px;
         float: right;
       }
-      &.dot{
+
+      &.dot {
         width: 50%;
       }
     }
