0499c35bdb19593fdc58674f6e0c6597d3cb4189
nicole <1251501326@qq.com>
2021-03-14 14:09:25 +0800

    'init'

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports ={
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/merney/'
+    ? '/merney-vue/'
     : '/',
   lintOnSave: false,
   chainWebpack:config =>{
