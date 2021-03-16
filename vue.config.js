a263fd7142a6dc05fae1211f5ee8ae8f6e606939
nicole <1251501326@qq.com>
2021-03-16 21:16:42 +0800

    '修改时间控件'

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports ={
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/merney-vue/'
+    ? '/merney/'
     : '/',
   lintOnSave: false,
   chainWebpack:config =>{
