a45851a080b8bd61f8e82e36ecf9777e4b858926
nicole <1251501326@qq.com>
2021-03-27 13:23:34 +0800

    'init'

---
+++ precache-manifest.91ad64e9ff2fd38734618bc5bcf51797.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a47d0e35bb0e047ef4b",
+    "url": "/merney-vue/css/app.bdacaa92.css"
+  },
+  {
+    "revision": "22910b193cbda770be35",
+    "url": "/merney-vue/css/chunk-vendors.90699438.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/merney-vue/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/merney-vue/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "2424afd42364083325da085bb44c7b80",
+    "url": "/merney-vue/index.html"
+  },
+  {
+    "revision": "3a47d0e35bb0e047ef4b",
+    "url": "/merney-vue/js/app.e6ff7116.js"
+  },
+  {
+    "revision": "22910b193cbda770be35",
+    "url": "/merney-vue/js/chunk-vendors.2e1251da.js"
+  },
+  {
+    "revision": "0403ecee74246d7ea57b3bac39c6701c",
+    "url": "/merney-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/merney-vue/robots.txt"
+  }
+]);
